da4209c16d1342b4b6842a7a0a23ddc821723004
Huey Lee <leejinhuey@gmail.com>
2017-09-21 20:29:54 +0800

    up to pattern matching

---
+++ pattern_matching.exs
@@ -0,0 +1,8 @@
+{a, b, c} = {:hello, "world", 42}
+IO.puts "a is #{a}"
+IO.puts "b is #{b}"
+[head | tail] = [1, 2, 3]
+IO.puts "head is #{head}"
+{:ok, result} = {:ok, 13}
+IO.puts "result is #{result}"
+
